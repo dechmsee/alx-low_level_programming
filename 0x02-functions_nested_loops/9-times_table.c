1f83fa472ca17ca762eafa0765a8c8348fc81293
Dennis <dmutuimatthews@gmail.com>
2022-07-06 08:24:15 +0000

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,8 +15,7 @@ for (y = 0; y <= 9; y++)
 {
 z = x * y;
 
-if (z > 9);
-
+if (z > 9)
 {
 u = z % 10;
 d = (z - u) / 10;
@@ -26,7 +25,6 @@ _putchar(32);
 _putchar(d + '0');
 _putchar(u + '0');
 }
-else
 {
 if (y != 0)
 {
