b3a47a880c4d9cf275743dd515ade062bb3ea198
dechmsee <dmutuimatthews@gmail.com>
2022-08-18 15:38:40 +0000

    function that prints a listint_t linked list.

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,30 +1,79 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a linked list
- * @head: list of type listint_t
+ * _get_loop - Counts the number of unique nodes
+ * @head: list
  *
- * Return: number of nodes
-*/
-
-size_t print_listint_safe(const listint_t *head)
+ * Return: nodes or 0
+ */
+size_t _get_loop(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	const listint_t *chaser, *runner;
+	size_t nodes = 1;
 
-	while (head)
+	if (!head || !head->next)
+		return (0);
+
+	chaser = head->next;
+	runner = (head->next)->next;
+
+	for (; runner;)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		if (chaser == runner)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			break;
+			chaser = head;
+			for (; chaser != runner;)
+			{
+				nodes++;
+				chaser = chaser->next;
+				runner = runner->next;
+			}
+
+			chaser = chaser->next;
+			for (; chaser != runner;)
+			{
+				nodes++;
+				chaser = chaser->next;
+			}
+
+			return (nodes);
 		}
+
+		chaser = chaser->next;
+		runner = (runner->next)->next;
 	}
 
-	return (num);
+	return (0);
+}
+
+/**
+ * print_listint_safe - Prints a list safely
+ * @head: list
+ *
+ * Return: The number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nodes, index = 0;
+
+	nodes = _get_loop(head);
+
+	if (!nodes)
+	{
+		for (; head; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		goto KILL;
+	}
+
+	for (index = 0; index < nodes; index++)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+	}
+	printf("-> [%p] %d\n", (void *)head, head->n);
+
+KILL:	return (nodes);
 }
